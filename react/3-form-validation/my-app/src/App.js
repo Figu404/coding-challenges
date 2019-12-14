065581dd84c53e3a31bef1157319a3d5285e1050
Figu404 <filip.h.strom@gmail.com>
2019-12-15 00:23:34 +0100

    I think i have gone the wrong way......

--- react/3-form-validation/my-app/src/App.js
+++ react/3-form-validation/my-app/src/App.js
@@ -5,13 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <legend>Användar information</legend>
-      <div>
-        <input type="text"/>
-        <input type="text"/>
-        <input type="text"/>
-        <input type="text"/>
-      </div>
+      <h1>Användar information</h1>
+      <form action="">
+        <label for="frmNameA">Name</label>
+        <input name="name" id="frmNameA" placeholder="Full name" required autocomplete="name"/>
+        <label for="Email">Email</label>
+        <input type="email" id="Email" placeholder="name@example.com" required autocomplete="email"/>
+        <label for="phoneNumber">Phone</label>
+        <input type="tel" id="phoneNumber" pattern="[0-9]{10}" placeholder="0000000000" required autocomplete="tel"/>
+        <input type="submit"></input>
+      </form>
 
     </div>
   );
