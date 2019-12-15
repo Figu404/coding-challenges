9cddcc0c2484b330347a5c3954e104d2106c5b7b
Figu404 <filip.h.strom@gmail.com>
2019-12-16 00:29:41 +0100

    React 3-form

--- react/3-form-validation/my-app/src/App.js
+++ react/3-form-validation/my-app/src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const[name, setName] = useState("");
+  function changeName(e){
+    setName(e.target.value)
+    console.log(e.target.value);
+    console.log(e.target.value==="f")
+    if (name==="f"){
+      console.log("inne!")
+      setName("e.target.value")
+    }
+    
+  }
   return (
     <div className="App">
       <h1>Användar information</h1>
       <form>
-        <label for="name">Name</label>
-        <input name="name" id="name" placeholder="Full name" required autocomplete="name"/>
-        <label for="Email">Email</label>
-        <input name="enail" type="email" id="Email" placeholder="name@example.com" required autocomplete="email"/>
-        <label for="phoneNumber">Phone</label>
-        <input name="phoneNumber" type="tel" id="phoneNumber" pattern="[0-9]{10}" placeholder="0000000000" required autocomplete="tel"/>
+        <label htmlFor="name">Name</label>
+        <input onChange={changeName} name="name" id="name" placeholder="Full name" required autoComplete="name"/>
+        <label htmlFor="Email">Email</label>
+        <input name="enail" type="email" id="Email" placeholder="name@example.com" required autoComplete="email"/>
+        <label htmlFor="phoneNumber">Phone</label>
+        <input name="phoneNumber" type="tel" id="phoneNumber" pattern="[0-9]{10}" placeholder="0000000000" required autoComplete="tel"/>
         <input type="Button"></input>
       </form>
+      <p>{name}</p>
     </div>
   );
 }
