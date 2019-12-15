8481376840cdf9d0e469ad4deea233c08299a70f
Figu404 <filip.h.strom@gmail.com>
2019-12-15 13:09:49 +0100

    on the rigth way again

--- react/3-form-validation/my-app/src/App.js
+++ react/3-form-validation/my-app/src/App.js
@@ -3,19 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const[name, setName] = useState("");
   return (
     <div className="App">
       <h1>Användar information</h1>
-      <form action="">
-        <label for="frmNameA">Name</label>
-        <input name="name" id="frmNameA" placeholder="Full name" required autocomplete="name"/>
+      <form>
+        <label for="name">Name</label>
+        <input name="name" id="name" placeholder="Full name" required autocomplete="name"/>
         <label for="Email">Email</label>
-        <input type="email" id="Email" placeholder="name@example.com" required autocomplete="email"/>
+        <input name="enail" type="email" id="Email" placeholder="name@example.com" required autocomplete="email"/>
         <label for="phoneNumber">Phone</label>
-        <input type="tel" id="phoneNumber" pattern="[0-9]{10}" placeholder="0000000000" required autocomplete="tel"/>
-        <input type="submit"></input>
+        <input name="phoneNumber" type="tel" id="phoneNumber" pattern="[0-9]{10}" placeholder="0000000000" required autocomplete="tel"/>
+        <input type="Button"></input>
       </form>
-
     </div>
   );
 }
